4b97b57db7d2acb6f15560c01bc71f54e63dd008
Victor Smirnov <vityaman.dev@yandex.ru>
2023-11-29 18:47:36 +0300

    added bash run script

--- ci/run.sh
+++ ci/run.sh
@@ -2,6 +2,15 @@ set -o errexit
 cd $(dirname -- "$0"; )
 cd ..
 
+MODE=$1
+if [[ $MODE == "debug" ]] then
+  echo "[xv6] Mode is 'gdb'" 
+  TARGET="qemu-gdb"
+else
+  echo "[xv6] Mode is 'run'" 
+  TARGET="qemu"
+fi 
+
 echo "[xv6] Running XV6 OS..."
-bear -- make qemu && make clean
+bear -- make $TARGET && make clean
 echo "[xv6] Done!"
